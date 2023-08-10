7bc408232af7bd97671ac6e560d0c7d62b97ed53
Jpwrbd <jpwrbd456@gmail.com>
2023-08-10 18:22:31 +0700

    up

--- Homework/HW03/hw03-2-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-2-codex-and-fix-bug.cpp
@@ -1,25 +1,38 @@
+// TODO: #9 Fix bug and arrange code in CODEX pattern.
 #include <stdio.h>
 
-int main() {
-    float a, b = 3 ;
-    bool _1 = 0 ;
-    printf( "%d " , _1) ;
+int main()
+{
+    float a, b = 3;
+    bool _1 = 0;
 
-    int i = 0 ;
-    if (_1 != 0) {
-        printf( "ok " ) ;
-    } else {
-        while (i <= 5) {
-            printf( " not-okay-%d " , ++i) ;
+    printf("%d ", _1);
+
+    int i = 0;
+    if (_1 != 0)
+    {
+        printf("ok ");
+    }
+    else
+    {
+        while (i <= 5)
+        {
+            printf(" not-okey-%d ", ++i);
         }
 
-        for (i = 3; i < 10; i += 1) {
-            if (i % 2 == (!1 ? 1 : 2)) {
-                printf( "see see" ) ;
+        for (i = 3; i < 10; i += 1)
+        {
+            if (i % 2 == (!1 ? 1 : 2))
+            {
+                printf("see see");
+            }
+            else
+            {
+                printf("haha");
             }
         }
     }
-    return 0 ;
+
+    return 0;
 }
-/*not-okey-1  not-okey-2  not-okey-3  not-okey-4  not-okey-5  not-okey-6 haha
-not-okay-1  not-okay-2  not-okay-3  not-okay-4  not-okay-5  not-okay-6 see seehahasee seehahasee seehahasee see*/
+/*not-okey-1  not-okey-2  not-okey-3  not-okey-4  not-okey-5  not-okey-6 haha */
